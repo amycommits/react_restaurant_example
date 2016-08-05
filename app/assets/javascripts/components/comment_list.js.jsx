bfcd0a727e04aa3c7d520f59ceafcf5023c56a0b
Amy Detwiler <detwiler.amy@gmail.com>
2016-08-05 13:30:26 -0400

    cleaned up variables with splat

--- app/assets/javascripts/components/comment_list.js.jsx
+++ app/assets/javascripts/components/comment_list.js.jsx
@@ -3,8 +3,8 @@ var CommentList = React.createClass({
 	render(){
 		return(
 			<div>
-				{JSON.parse(this.props.comments).map(function(feedback){
-					return <Comment key={feedback.id} rank={feedback.rank} comment={feedback.comment} user_id={feedback.user_id}/>;
+				{JSON.parse(this.props.comments).map(function(comment){
+					return <Comment key={comment.id} {...comment}/>;
 				})}
 			</div>
 		);
